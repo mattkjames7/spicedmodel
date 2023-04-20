642b85ab4481c4eb928ead07fde7b81ec686ed82
Matt <mattkjames7@gmail.com>
2023-04-20 14:48:12 +0100

    removed old code

--- spicedmodel/__data/liblsmodel/tests/matdot.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MATDOT_H__
-#define __MATDOT_H__
-#include <stdio.h>
-#include <stdlib.h>
-#include "../matrix.h"
-#include "../matrixmath.h"
-#endif
-
-void testMatDot();
--- spicedmodel/__data/liblsmodel/tests/testmavtrans.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TESTMAVTRANS_H__
-#define __TESTMAVTRANS_H__
-#include <stdio.h>
-#include <stdlib.h>
-#include "../mavtrans.h"
-
-#endif
-void testMavTrans();
